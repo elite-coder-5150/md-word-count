99f95b377c4969032c3940d9db49f303936beab4
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-04-08 06:33:09 -0500

    simple count the words of a file

---
+++ count_words.js
@@ -0,0 +1,25 @@
+document.getElementById('fileInput').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const text = e.target.result;
+        const wordCount = countWords(text);
+        const characterCount = countCharacters(text);
+
+        console.log(`Word count: ${wordCount}`);
+        console.log(`Character count: ${characterCount}`);
+    };
+
+    reader.readAsText(file);
+});
+
+function countWords(text) {
+    const words = text.trim().split(/\s+/);
+    return words.length;
+}
+
+function countCharacters(text) {
+    text = text.replace(/[*_#`~[\]()<>-]/g, '');
+    return text.length;
+}
